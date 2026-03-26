00b2d082b3114b1018e9bb42a35120262c700aed
Viet Hoang <gvhoang30112004@gmail.com>
2026-03-26 18:07:01 +0700

    Add IsSubsequence solution implementation

---
+++ easy/IsSubsequence.java
@@ -0,0 +1,28 @@
+package easy;
+
+
+//Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+//A subsequence of a string is a new string that is formed from the original string by deleting some
+//        (can be none) of the characters without disturbing the relative positions of the remaining characters.
+//        (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+public class IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int ls = 0;
+        int sSize = s.length();
+        for ( int i = 0; i < t.length(); i++ ){
+            if ( ls < sSize && s.charAt(ls) == t.charAt(i)){
+                ls++;
+            }
+        }
+        return ls == s.length();
+    }
+
+    public static void main(String[] args) {
+//        Input: s = "abc", t = "ahbgdc"
+//        Output: true
+         String s = "abc";
+         String t = "ahbgdc";
+         System.out.println( new IsSubsequence().isSubsequence(s, t));
+    }
+}
