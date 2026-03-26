eb21fe4405ce945dc7e5d51d1fc8a8eda0461263
Viet Hoang <gvhoang30112004@gmail.com>
2026-03-26 17:57:21 +0700

    Add LongestCommonPrefix solution implementation

---
+++ easy/Array/LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package easy.Array;
+
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for ( int i = 1; i < strs.length; i++ ){
+            while (!strs[i].startsWith(prefix)){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+//        Input: strs = ["flower","flow","flight"]
+//        Output: "fl"
+        String[] strs = {"flower","flow","flight"};
+        System.out.println( new LongestCommonPrefix().longestCommonPrefix(strs));
+    }
+}
